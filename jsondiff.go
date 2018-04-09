728d6fd914c32d45d2f96981e78dd4ff879126fe
Scott Ranger <millenniumnight@googlemail.com>
2018-04-09 17:44:27 +0000

    stash

--- jsondiff.go
+++ jsondiff.go
@@ -2,6 +2,17 @@ package jsondiff
 
 import "io"
 import "encoding/json"
+import "fmt"
+import "reflect"
+
+type Change string
+
+const (
+	Added     Change = "Added"
+	Updated   Change = "Updated"
+	Removed   Change = "Removed"
+	Unchanged Change = "Unchanged"
+)
 
 func Decode(r io.Reader) (map[string]interface{}, error) {
 	d := json.NewDecoder(r)
@@ -17,32 +28,145 @@ func Decode(r io.Reader) (map[string]interface{}, error) {
 }
 
 func KeysFromMap(m map[string]interface{}) []string {
-	i := 0
-	keys := make([]string, len(m))
+	s := make([]string, 0, len(m))
 
 	for k := range m {
-		keys[i] = k
-		i++
+		s = append(s, k)
+	}
+
+	return s
+}
+
+func minLength(l, r []string) int {
+	if ll, rl := len(l), len(r); ll < rl {
+		return ll
+	} else {
+		return rl
+	}
+}
+
+func AlignKeys(lks, rks []string) []string {
+	seen := make(map[string]struct{})
+	ml := minLength(lks, rks)
+
+	keys := make([]string, 0, ml)
+
+	for _, key := range lks {
+		if _, ok := seen[key]; !ok {
+			seen[key] = struct{}{}
+			keys = append(keys, key)
+		}
+	}
+
+	for _, key := range rks {
+		if _, ok := seen[key]; !ok {
+			seen[key] = struct{}{}
+			keys = append(keys, key)
+		}
 	}
 
 	return keys
 }
 
-func AlignKeys(leftKeys, rightKeys []string, ch chan string) {
-	allKeysCount := len(leftKeys) + len(rightKeys)
-	combinedKeys := make([]string, 0, allKeysCount)
+type Pair struct {
+	Key    string      `json:"key"`
+	Left   interface{} `json:"left"`
+	Right  interface{} `json:"right"`
+	Change Change      `json:"change,omitempty"`
+}
 
-	combinedKeys = append(combinedKeys, leftKeys...)
-	combinedKeys = append(combinedKeys, rightKeys...)
+func (p Pair) String() string {
+	return fmt.Sprintf("{Key: %v Left: %v Right: %v Change: %v}", p.Key, p.Left, p.Right, p.Change)
+}
 
-	seen := make(map[string]struct{})
+type Tree struct {
+	Root     *Pair   `json:"root"`
+	Children []*Tree `json:"children,omitempty"`
+}
 
-	for _, key := range combinedKeys {
-		if _, ok := seen[key]; !ok {
-			seen[key] = struct{}{}
-			ch <- key
-		}
+func (t *Tree) String() string {
+	res := fmt.Sprintf("%v\n", t.Root.String())
+
+	for _, p := range t.Children {
+		res = fmt.Sprintf("%v  %v", res, p)
 	}
 
-	close(ch)
+	return res
+}
+
+func Diff(left, right map[string]interface{}) *Tree {
+	tree := new(Tree)
+
+	diff(tree, left, right, "")
+
+	return tree
+}
+
+func diff(tree *Tree, left, right map[string]interface{}, rootKey string) *Tree {
+	lks := KeysFromMap(left)
+	rks := KeysFromMap(right)
+	aks := AlignKeys(lks, rks)
+
+	tree.Root = &Pair{
+		Key:   rootKey,
+		Right: right,
+		Left:  left,
+	}
+
+	for _, key := range aks {
+		rv := right[key]
+		lv := left[key]
+		lt := reflect.TypeOf(lv)
+		rt := reflect.TypeOf(rv)
+		if rt == nil && lt == nil {
+			tree.Children = append(tree.Children, &Tree{Root: &Pair{
+				Key:    key,
+				Change: Unchanged,
+				Left:   nil,
+				Right:  nil,
+			}})
+		} else if rt == nil {
+			tree.Children = append(tree.Children, &Tree{Root: &Pair{
+				Key:    key,
+				Change: Removed,
+				Left:   lv,
+				Right:  nil,
+			}})
+		} else if lt == nil {
+			tree.Children = append(tree.Children, &Tree{Root: &Pair{
+				Key:    key,
+				Change: Added,
+				Left:   nil,
+				Right:  rv,
+			}})
+		} else if lt != rt {
+			tree.Children = append(tree.Children, &Tree{Root: &Pair{
+				Key:    key,
+				Change: Updated,
+				Left:   lv,
+				Right:  rv,
+			}})
+		} else if lt.Kind() == reflect.Map {
+			tree.Children = append(tree.Children, diff(new(Tree), lv.(map[string]interface{}), rv.(map[string]interface{}), key))
+		} else if lv != rv {
+			tree.Children = append(tree.Children, &Tree{Root: &Pair{
+				Key:    key,
+				Change: Updated,
+				Left:   lv,
+				Right:  rv,
+			}})
+		} else {
+			tree.Children = append(tree.Children, &Tree{Root: &Pair{
+				Key:    key,
+				Change: Unchanged,
+				Left:   lv,
+				Right:  rv,
+			}})
+		}
+
+	}
+
+	// tree change overall=
+
+	return tree
 }
--- jsondiff_test.go
+++ jsondiff_test.go
@@ -1,8 +1,10 @@
 package jsondiff
 
 import "testing"
-import "strings"
+import "os"
 import "reflect"
+import "sort"
+import "strings"
 
 func TestDecode(t *testing.T) {
 	r := strings.NewReader(`{ "foo": "bar" }`)
@@ -24,21 +26,18 @@ func TestDecode(t *testing.T) {
 }
 
 func TestAlignKeys(t *testing.T) {
-	keysA := []string{"foo", "bar"}
-	keysB := []string{"bar", "baz"}
+	lks := []string{"foo", "bar"}
+	rks := []string{"bar", "baz"}
+
 	expected := []string{"foo", "bar", "baz"}
-	ch := make(chan string)
-
-	go AlignKeys(keysA, keysB, ch)
-
-	got := make([]string, 0, 3)
-	for key := range ch {
-		got = append(got, key)
-	}
+	got := AlignKeys(lks, rks)
 
 	if l := len(got); l != 3 {
 		t.Errorf("Wrong number of elements:\ngot: %v, want: %v\n", l, 3)
 	}
+
+	sort.Strings(got)
+	sort.Strings(expected)
 
 	eq := reflect.DeepEqual(got, expected)
 
@@ -56,11 +55,57 @@ func TestKeysFromMap(t *testing.T) {
 
 	expected := []string{"foo", "bar", "baz"}
 
-	keys := KeysFromMap(m)
+	got := KeysFromMap(m)
 
-	eq := reflect.DeepEqual(keys, expected)
+	eq := reflect.DeepEqual(got, expected)
 
 	if !eq {
-		t.Errorf("Wrong result:\ngot: %v\nwant: %v\n", keys, expected)
+		t.Errorf("Wrong result:\ngot: %v\nwant: %v\n", got, expected)
 	}
+}
+
+func TestDiff(t *testing.T) {
+	diffLeft, err := os.Open("testdata/diffleft.json")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer diffLeft.Close()
+
+	diffRight, err := os.Open("testdata/diffright.json")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer diffRight.Close()
+
+	diffResult, err := os.Open("testdata/diffresult.json")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer diffResult.Close()
+
+	leftMap, err := Decode(diffLeft)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	rightMap, err := Decode(diffRight)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	tree := Diff(leftMap, rightMap)
+
+	t.Errorf("\n\n%v\n\n", tree)
 }
